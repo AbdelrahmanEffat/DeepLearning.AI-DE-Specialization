41561aa32193a5332ee30119a1d2bddf14f69abd
AbdelrahmanEffat <abdelrahman_effat@outlook.com>
2025-11-16 18:43:18 +0000

    Add C4 W4 Assignment Part2: Capstone Project Part 2 - Data Quality and Orchestration and Visualization

---
+++ dbt_modeling/models/bi_views/sales_per_country_vw.sql
@@ -0,0 +1,8 @@
+SELECT
+date_part('month', fs.session_start_time) AS session_month,
+date_part('year', fs.session_start_time) AS session_year,
+du.country_code,
+SUM(fs.price) AS total_sales
+FROM {{var("target_schema")}}.fact_session fs
+LEFT JOIN {{var("target_schema")}}.dim_users du
+ON fs.us
